8a98aa690d018d26fc66e77e0d68752556388d31
Chumphon <chumphon.aekwarodom@scbtechx.io>
2024-02-25 17:42:47 +0700

    add: api GET /lotteries and unit test

--- posttest/src/main/java/com/kbtg/bootcamp/posttest/lottery/LotteryController.java
+++ posttest/src/main/java/com/kbtg/bootcamp/posttest/lottery/LotteryController.java
@@ -6,6 +6,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,6 +33,13 @@ public class LotteryController {
         AddLotteryResponse addedAddLotteryResponse = lotteryService.addTicket(request);
 
         return new ResponseEntity<>(addedAddLotteryResponse, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/lotteries")
+    public ResponseEntity<GetAllTicketResponse> getAllTicket(){
+        GetAllTicketResponse getAllTicketResponse = lotteryService.getAllTicket();
+
+        return new ResponseEntity<>(getAllTicketResponse, HttpStatus.OK);
     }
 
 }

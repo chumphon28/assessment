e637a866e9e0c9bf47fc1c77dba8b2f020916da5
Chumphon <chumphon.aekwarodom@scbtechx.io>
2024-02-26 02:47:29 +0700

    add: basic auth to POST /admin/lotteries

--- posttest/src/main/java/com/kbtg/bootcamp/posttest/exception/ErrorResponse.java
+++ posttest/src/main/java/com/kbtg/bootcamp/posttest/exception/ErrorResponse.java
@@ -1,10 +1,12 @@
 package com.kbtg.bootcamp.posttest.exception;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
+@AllArgsConstructor
 public class ErrorResponse {
     private String code;
     private String message;
